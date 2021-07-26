7dac8f20620669a65ba47ddef7bc4c02be6c9a3b
aren227 <mathdong01@gmail.com>
2021-07-26 17:07:05 +0900

    기본 폰트 변경

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ const theme = extendTheme({
       },
     },
   },
+  fonts: {
+    heading: "Noto Sans KR, sans-serif",
+    body: "Noto Sans KR, sans-serif",
+  },
 });
 
 function MyApp({ Component, pageProps }) {
